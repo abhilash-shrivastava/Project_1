665e7df49b91fcf9fe36ce37af716d9ab635a62b
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-10-08 18:36:22 -0700

    correct the aws ip

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 echo "deploying to aws"
 
 # log into aws everything under this is executed under the ubuntu user
-ssh ubuntu@ec2-52-88-156-243.us-west-2.compute.amazonaws.com <<ubuntu
+ssh ubuntu@ec2-54-211-23-124.compute-1.amazonaws.com <<ubuntu
     echo "SSH connection done"
 ubuntu

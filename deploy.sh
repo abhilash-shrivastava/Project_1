657381afe046fe5209772e5883296d09a7adf897
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-10-08 20:24:46 -0700

    changed host to public for webpack dev server

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,6 @@ ssh -v ubuntu@ec2-54-167-25-16.compute-1.amazonaws.com <<ubuntu
     echo "SSH connection done"
     cd ~/apps/Project_1
     sudo git pull github deploy
-    sudo apt-get update
-    sudo apt-get install nodejs
-    sudo apt-get install npm
     sudo npm install
     sudo npm start
 ubuntu

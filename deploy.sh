a8c56fcf5add4aa2798558ad0fc74d2a1927ff31
Abhilash Shrivastava <ashrivas@mail.sfsu.edu>
2016-10-10 22:17:42 -0700

    ec2 instance IP change

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo "deploying to aws"
 
 # log into aws everything under this is executed under the ubuntu user
-ssh -v ubuntu@ec2-54-167-25-16.compute-1.amazonaws.com <<ubuntu
+ssh -v ubuntu@ec2-54-209-196-250.compute-1.amazonaws.com <<ubuntu
     echo "SSH connection done"
     cd ~/apps/Project_1
     git pull github master
